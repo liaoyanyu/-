83dad835f1e5c6272a134ca5ed805b571cd7295c
liaoyanyu <1440913180@qq.com>
2021-08-11 15:03:56 +0800

    c

---
+++ str1.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<assert.h>
+
+//strlen
+//strcpy
+//void my_strcpy(char*dest,const char*src)
+//{
+//	assert(dest!=NULL);
+//	assert(src!=NULL);
+//	while(*src != '\0')
+//	{
+//		*dest=*src;
+//		dest++;
+//		src++;
+//	}
+//	*dest=*src;//¿½±´\0
+//}
+//int main()
+//{
+//	char arr1[]="abcdefghi";
+//	char arr2[]="bit";
+//	//strcpy(arr1,arr2);
+//	my_strcpy(arr1,arr2);
+//;	printf("%s\n",arr1);
+//	return 0;
+//}
+
+
+//strcat--×·¼Ó×Ö·û´®
+
+//char* my_strcat(char* dest,const char* src)
+//{
+//	char* ret=dest;
+//	//assert(dest && src);//!=NULL
+//	int i=0;
+//	while(*dest!='\0')
+//	{
+//		dest++;
+//	}
+//	while(*dest++=*src++)
+//	{
+//		;
+//	}
+//	return ret;
+//
+//}
+//int main()
+//{
+//	char arr1[30]={"hello"};
+//	char arr2[]={"world"};
+//	//strcat(arr1,arr2);
+//	my_strcat(arr1,arr2);
+//	printf("%s\n",arr1);
+//	return 0;
+//}
+
+
+
+
+
+
